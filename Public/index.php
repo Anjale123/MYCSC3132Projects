4a33af0189f35f4226d91409152bc0c4c5ba5d79
Anjalee Kaushalya <139565339+Anjale123@users.noreply.github.com>
2024-11-30 07:42:26 +0000

    edit index file

--- Public/index.php
+++ Public/index.php
@@ -8,14 +8,32 @@ try {
 	$sql = "SELECT * FROM student";
 
 	//excute the quey
+
 	$result = mysqli_query($connect,$sql);
 	//check if data exists in the table
+
 	if (mysqli_num_rows($result)>0) {
-		// fetch the data from rows
-		while ($row = mysqli_fetch_assoc($result)) {
-			print_r($row);
+		echo "<table border='1'>";
+		$col=mysqli_fetch_fields($result);
+		echo "<tr>";
+
+		foreach($col as $value){
+			echo "<td>$value->name</td>";
 		}
-	} else {
+		echo "</tr>";
+
+		while($row=mysqli_fetch_assoc($result)){
+			echo "<tr>";
+
+			foreach($row as $key =>$value){
+				echo "<td>$value</td>";
+			}
+			echo "</tr>";
+		}
+		echo "</table>";
+
+	} 
+	else {
 		echo "No results";
 	}
 	
@@ -23,6 +41,3 @@ try {
 catch (Exception $e) {
 	die($e->getMessage());
 }
-
-
-?>
