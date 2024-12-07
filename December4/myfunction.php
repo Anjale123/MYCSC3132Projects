be1c4071a1f780ee0988c84cfa64a816aa09d4c7
Anjalee Kaushalya <139565339+Anjale123@users.noreply.github.com>
2024-12-07 17:10:22 +0000

    add myfunction page

---
+++ December4/myfunction.php
@@ -0,0 +1,106 @@
+<?php 
+function PrintTable($student,$connect)
+{
+	try {
+		$sql = "SELECT * FROM $student";
+		$result = mysqli_query($connect,$sql);
+	
+		if (mysqli_num_rows($result)>0) {
+		
+			echo "<table border='1'>";
+			$col = mysqli_fetch_fields($result);
+		
+			echo "<tr>";
+			foreach ($col as $value) {
+			
+				echo "<td>$value->name</td>";
+			}
+			echo "</tr>";
+			
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+				foreach ($row as $key => $value) {
+					echo "<td>$value</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "No results";
+		}
+		
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+}
+function PrintTableCols($student,$connect,$name)
+{
+	try {
+
+		$sql = "SELECT ";
+		for ($i=0; $i < sizeof($name)-1; $i++) { 
+			$sql .=$name[$i].",";
+			//echo $sql;
+		}
+		$sql .=$name[sizeof($name)-1]." from $student";
+
+		$result = mysqli_query($connect,$sql);
+		if (mysqli_num_rows($result)>0) {
+			echo "<table border='1'>";
+			$col = mysqli_fetch_fields($result);
+			echo "<tr>";
+			foreach ($col as $value) {
+				echo "<td>$value->name</td>";
+			}
+			echo "</tr>";
+			
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+				foreach ($row as $key => $value) {
+					echo "<td>$value</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "No results";
+		}
+		
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+}
+
+function Searchname($Male,$connect)
+{
+	try {
+
+		$sql = "SELECT * FROM student where gender like '%$Male%' ";
+		$result = mysqli_query($connect,$sql);
+		if (mysqli_num_rows($result)>0) {
+			echo "<table border='1'>";
+			$col = mysqli_fetch_fields($result);
+			echo "<tr>";
+			foreach ($col as $value) {
+				echo "<td>$value->name</td>";
+			}
+			echo "</tr>";
+			
+			while ($row = mysqli_fetch_assoc($result)) {
+				echo "<tr>";
+				foreach ($row as $key => $value) {
+					echo "<td>$value</td>";
+				}
+				echo "</tr>";
+			}
+			echo "</table>";
+		} else {
+			echo "No results";
+		}
+		
+	} catch (Exception $e) {
+		die($e->getMessage());
+	}
+}
+
+?>
